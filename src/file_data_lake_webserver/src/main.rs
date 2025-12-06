eaf2919f78e3eeade5152482b03900ab39a709bd
Michael Nenninger <roboter@mine-robo.de>
2025-12-06 18:23:47 +0100

    add static files for web server

--- src/file_data_lake_webserver/src/main.rs
+++ src/file_data_lake_webserver/src/main.rs
@@ -1,7 +1,9 @@
+use actix_files as fs;
+
 //https://github.com/actix/examples
 use actix_web::{get, web, App, HttpServer, Responder, middleware::Logger};
 
-#[get("/hello/{name}")]
+#[get("/api/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {}!", name)
 }
@@ -18,6 +20,11 @@ async fn main() -> std::io::Result<()> {
             // Add the Logger middleware to log all incoming requests.
             .wrap(Logger::new("%a %{User-Agent}i"))
             .service(greet)
+            .service(fs::Files::new("", "./wwwroot")
+                .use_last_modified(true)
+                .use_etag(true)
+                .prefer_utf8(true)
+                .index_file("index.html"))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
