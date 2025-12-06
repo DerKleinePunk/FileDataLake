1232ada51d53429b7f0f3d63455008b3b2341907
Plikoener <dev@plikoener.de>
2025-12-06 22:25:00 +0100

    add showUpload, rename file

---
+++ src/wwwroot/upload.js
@@ -0,0 +1,8 @@
+function uploadFile(){
+    return}
+
+function showUpload(){
+    document.getElementById("main-container").hidden = true;
+    document.getElementById("upload-container").hidden = false;
+    return;
+}
